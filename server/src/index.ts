2f3d31edacfa0966ff225939870cfab75042ed96
SubarnaKhadka <subarna31k@gmail.com>
2023-04-26 22:29:34 +0545

    Chat send/receive Message implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,52 @@ const cors = require("cors");
 const app = require("express")();
 const httpServer = require("http").createServer(app);
 const { Server } = require("socket.io");
+interface ISocket {
+  on: (event: string, callback: (args: any) => void) => void;
+  id: string;
+}
 
 app.use(cors());
-const socket_io = new Server(httpServer, {
+const socketIO = new Server(httpServer, {
   cors: {
     origin: `http://localhost:3000`,
     methods: ["GET", "POST"],
   },
 });
 
-socket_io.on("connection", (socket: { id: string }) => {
-  console.log(socket.id);
+let users: Array<{ userId: string; socketId: string }> = [];
 
-  socket_io.on("disconnect", (socket: { id: string }) => {
-    console.log(`user disconnected ${socket.id}`);
+const addUserToSocket = (userId: string, socketId: string) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUserFromSocket = (socketId: string) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUserFromSocket = (userId: string) => {
+  return users.find((user) => user.userId === userId);
+};
+
+socketIO.on("connection", (socket: ISocket) => {
+  socket.on("addUserToSocket", (userId) => {
+    addUserToSocket(userId as string, socket.id);
+    socketIO.emit("getSocketUsers", users);
+  });
+
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUserFromSocket(receiverId as string);
+    socketIO.to(user?.socketId).emit("getMessage", {
+      senderId: senderId as string,
+      text: text as string,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected: ${socket.id}`);
+    removeUserFromSocket(socket.id);
+    socketIO.emit("getSocketUsers", users);
   });
 });
 
